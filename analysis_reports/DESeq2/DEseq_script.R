f860903a70ea817db015185b78089966ed8a6069
Matthew Berginski <matthew.berginski@gmail.com>
2020-11-02 18:18:48 -0500

    removing line comments

--- analysis_reports/DESeq2/DEseq_script.R
+++ analysis_reports/DESeq2/DEseq_script.R
@@ -9,16 +9,16 @@ library(tictoc)
 library(readr)
 library(here)
 
-register(MulticoreParam(10))
+register(MulticoreParam(64))
 
 tic();
 
 drug_perturb_exp = summarizeToGene(tximeta(read_csv(here('analysis_reports/DESeq2/CRISPRi_exp_info.csv'))))
 
-# dds <- DESeqDataSet(drug_perturb_exp, design = ~group)
-# keep <- rowSums(counts(dds)) > 1
-# dds <- dds[keep,]
-# 
-# dds_analysis <- DESeq(dds)
-# write_rds(dds_analysis, here('DESeq_results/DEseq_analysis.rds'))
+dds <- DESeqDataSet(drug_perturb_exp, design = ~group)
+keep <- rowSums(counts(dds)) > 1
+dds <- dds[keep,]
+
+dds_analysis <- DESeq(dds)
+write_rds(dds_analysis, here('DESeq_results/DEseq_analysis.rds'))
 toc();
